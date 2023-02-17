f956c878d3e7a00bd5e18cb3decf79c0aaaef4d7
石井健太 <kenta.ishii@thinkings.co.jp>
2023-02-17 17:14:59 +0900

    TypeScript で動くところまで実装した。

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+const Square: React.FunctionComponent<{}> = (props) => {
+  return <a>Hello World.</a>;
+}
+
+export {Square};
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./styles.css";
+
+import {Square} from "./App";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Square />
+    </React.StrictMode>,
+    document.getElementById("root")
+)
