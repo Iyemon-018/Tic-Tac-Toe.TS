19df4e0b1059cf1b0c53326a6d09cbacbc31b037
石井健太 <kenta.ishii@thinkings.co.jp>
2023-02-20 09:47:12 +0900

    Why immutability is important まで実装した。

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,28 @@ const Square: React.FunctionComponent<{ value: string, onSquareClick: React.Mous
 const Board: React.FunctionComponent<{}> = () => {
   const [squares, setSquares] = useState<Array<string>>(Array(9).fill(null));
 
+  function handleSquareClick(i: number): void {
+    const nextSquares: Array<string> = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleSquareClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleSquareClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleSquareClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleSquareClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleSquareClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleSquareClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleSquareClick(8)} />
       </div>
     </>
   );
